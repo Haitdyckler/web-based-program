65eb312e3cc7588a4ea4c21119457b0d4925bc7e
Haitdyckler <hardy78912345@gmail.com>
2024-11-05 22:04:25 +0800

    Delete server directory

--- server/routes/questions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const questions = require('../models/questions.json');
-
-router.get('/', (req, res) => {
-  res.json(questions);
-});
-
-module.exports = router;
